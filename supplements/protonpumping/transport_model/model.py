e85e4b539ed11bd02acddc5bb546f85ee5aa106f
Oscar Branson <oscarbranson@gmail.com>
2026-01-13 11:40:56 +0000

    add f_DIC and documentation

--- supplements/protonpumping/transport_model/model.py
+++ supplements/protonpumping/transport_model/model.py
@@ -7,10 +7,56 @@ import warnings
 warnings.simplefilter("ignore")
 
 
-def constant_DIC(chamber_diameter_um, temperature, pHtot, J=None, pCO2=420, salinity=35, Ca_SW=10.2e-3, f_Ca=1, TE_SW=52e-3, F_M=0.0004, K_TE_G=2e-2, K_TE_J=0, G_k=1e-14, G_n=1.2, wall_thickness_um=3, hrs_to_run=12, method='Radau', rtol=1e-5, **solve_kwargs):
+def constant_DIC(chamber_diameter_um, temperature, pHtot, J=None, pCO2=420, salinity=35, Ca_SW=10.2e-3, f_Ca=1, TE_SW=52e-3, F_M=0.0004, K_TE_G=2e-2, K_TE_J=0, G_k=1e-14, G_n=1.2, f_DIC=1, wall_thickness_um=3, hrs_to_run=12, method='Radau', rtol=1e-5, **solve_kwargs):
+    """
+    Simple transport model of calcifying fluid chemistry under constant DIC conditions.
+    
+    Parameters
+    ----------
+    chamber_diameter_um : float
+        Diameter of the calcifying chamber in micrometers.
+    temperature : float
+        Temperature in degrees Celsius.
+    pHtot : float
+        Total scale pH of the seawater.
+    J : float, optional
+        Proton pumping rate in mol cm-2 s-1. If None, a temperature-dependent default is used.
+    pCO2 : float, optional
+        Partial pressure of CO2 in seawater in ppm. Default is 420 ppm. Used to calculate external water carbonate system.
+    salinity : float, optional
+        Salinity of the seawater. Default is 35. Used to calculate external water carbonate system.
+    Ca_SW : float, optional
+        Calcium concentration in seawater in mol kg-1. Default is 10.2ee-3 mol kg-1.
+    f_Ca : float, optional
+        Fraction of pumped protons that are balanced by calcium ions. Default is 1.
+    TE_SW : float, optional
+        Trace element concentration in seawater in mol kg-1. Default is 52e-3 mol kg-1 for Mg.
+    F_M : float, optional
+        Flushing rate of the calcifying fluid in s-1. Default is 0.0004 s-1 (40 minutes).
+    K_TE_G : float, optional
+        Partition coefficient for trace element during calcification. Default is 2e-2 for Mg.
+    K_TE_J : float, optional
+        Partition coefficient for trace element during Ca counter-transport associated with proton pumping. Default is 0.
+    G_k : float, optional
+        Pre-exponential factor for calcification rate. Default is 1e-14 mol cm-2 s-1.
+    G_n : float, optional
+        Exponent for saturation state dependence of calcification rate. Default is 1.2.
+    f_DIC : float, optional
+        Factor by which DIC is elevated in the calcifying fluid relative to seawater. Default is 1.
+    wall_thickness_um : float, optional
+        Thickness of the calcifying chamber wall in micrometers. Default is 3 um.
+    hrs_to_run : float, optional
+        Duration of the simulation in hours. Default is 12 hours.
+    method : str, optional
+        Integration method for solve_ivp. Default is 'Radau'.
+    rtol : float, optional
+        Relative tolerance for the ODE solver. Default is 1e-5.
+    **solve_kwargs : additional keyword arguments
+        Additional arguments passed to solve_ivp.
+    """
     
     initial_csys = cb.Csys(pHtot=pHtot, pCO2=pCO2, S_in=salinity, T_in=temperature, unit='mol')
-    DIC = initial_csys.DIC.item()
+    DIC = initial_csys.DIC.item() * f_DIC
 
     chamber_radius_cm = chamber_diameter_um / 2 * 1e-4  # cm
     chamber_surface_area_cm2 = 4 * np.pi * chamber_radius_cm**2 / 2  # cm2
@@ -64,7 +110,7 @@ def constant_DIC(chamber_diameter_um, temperature, pHtot, J=None, pCO2=420, sali
         return [dTA.item(), dCa, dTE]
 
 
-    sol = solve_ivp(transport_model, (0, hrs_to_run * 60 * 60), [initial_csys.TA.item(), initial_csys.Ca.item(), TE_SW], method=method, rtol=rtol, **solve_kwargs)
+    sol = solve_ivp(transport_model, (0, hrs_to_run * 60 * 60), [initial_csys.TA, initial_csys.Ca, TE_SW], method=method, rtol=rtol, **solve_kwargs)
     
     out = {k: v for k, v in zip(['TA', 'Ca', 'TE'], sol.y)}
     out['t'] = sol.t
@@ -90,10 +136,10 @@ def constant_DIC(chamber_diameter_um, temperature, pHtot, J=None, pCO2=420, sali
     return out
 
 
-def constant_DIC_rate(chamber_diameter_um, temperature, pHtot, J=None, pCO2=420, salinity=35, Ca_SW=10.2e-3, f_Ca=1,TE_SW=52e-3, F_M=0.0004, K_TE_G=2e-2, K_TE_J=0, G_k=1e-14, G_n=1.2, G_mid=2e-9/2, wall_thickness_um=3, hrs_to_run=12, method='Radau', rtol=1e-5, **solve_kwargs):
+def constant_DIC_rate(chamber_diameter_um, temperature, pHtot, J=None, pCO2=420, salinity=35, Ca_SW=10.2e-3, f_Ca=1,TE_SW=52e-3, F_M=0.0004, K_TE_G=2e-2, K_TE_J=0, G_k=1e-14, G_n=1.2, G_mid=2e-9/2, f_DIC=1, wall_thickness_um=3, hrs_to_run=12, method='Radau', rtol=1e-5, **solve_kwargs):
     
     initial_csys = cb.Csys(pHtot=pHtot, pCO2=pCO2, S_in=salinity, T_in=temperature, unit='mol')
-    DIC = initial_csys.DIC.item()
+    DIC = initial_csys.DIC.item() * f_DIC
 
     chamber_radius_cm = chamber_diameter_um / 2 * 1e-4  # cm
     chamber_surface_area_cm2 = 4 * np.pi * chamber_radius_cm**2 / 2  # cm2
@@ -149,7 +195,7 @@ def constant_DIC_rate(chamber_diameter_um, temperature, pHtot, J=None, pCO2=420,
         return [dTA.item(), dCa, dTE]
 
 
-    sol = solve_ivp(transport_model, (0, hrs_to_run * 60 * 60), [initial_csys.TA.item(), initial_csys.Ca.item(), TE_SW], method=method, rtol=rtol, **solve_kwargs)
+    sol = solve_ivp(transport_model, (0, hrs_to_run * 60 * 60), [initial_csys.TA, initial_csys.Ca, TE_SW], method=method, rtol=rtol, **solve_kwargs)
     
     out = {k: v for k, v in zip(['TA', 'Ca', 'TE'], sol.y)}
     out['t'] = sol.t
